52449198010040460567eccf7a1e01b2ad331ee4
Victor Glazer <victor.glazer@gmail.com>
2017-04-24 23:39:53 -0400

    Fixed glitches

--- assignment2/common.h
+++ assignment2/common.h
@@ -1,17 +1,14 @@
 #ifndef _COMMON_H_
 /********************************************************/
-/* common.h							*/
-/* common definitions that are used throughout the	*/
-/* compiler						*/
-/*							*/
+/* common.h                                             */
+/* common definitions that are used throughout the      */
+/* compiler                                             */
+/*                                                      */
 /********************************************************/
 #define _COMMON_H_
 
-static char commonVersion[] =
-	"$Id: common.h,v 1.1 2003/01/17 19:49:05 dw Exp $" ;
-
 /************************************************/
-/*  C libraries that are widely used		*/
+/*  C libraries that are widely used            */
 /************************************************/
 
 #include <stdio.h>
@@ -38,26 +35,26 @@ static char commonVersion[] =
 #endif
 
 /************************************************/
-/*  Limits on the source language.		*/
-/*  Enforced in scanner.c			*/
+/*  Limits on the source language.              */
+/*  Enforced in scanner.c                       */
 /************************************************/
 
    /*********************************************/
-   /*  Limit on size of identifier		*/
+   /*  Limit on size of identifier              */
    /*********************************************/
 
 #define  MAX_IDENTIFIER  32
 
    /*********************************************/
-   /*  Limit on size of text (string)		*/
+   /*  Limit on size of text (string)           */
    /* 255 characters plus 1 for terminating null*/
    /*********************************************/
 
 #define  MAX_TEXT   256
 
    /*********************************************/
-   /*  Limit on value of integer constant	*/
-   /*  16 bit integers				*/
+   /*  Limit on value of integer constant       */
+   /*  16 bit integers                          */
    /*********************************************/
 
 #define  MAX_INTEGER   32767
