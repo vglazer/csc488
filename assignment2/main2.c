8dac7ba2fad2a90aea25acd26e717dd292a32fa6
Victor Glazer <victor.glazer@gmail.com>
2017-04-26 09:56:44 -0400

    retab

--- assignment2/main2.c
+++ assignment2/main2.c
@@ -1,13 +1,13 @@
 /************************************************************************/
-/*									                                    */
-/*	main2.c								                                */
-/*	This is the main driver program for  CSC488S Assignment 2	        */
-/*	It handles compiler command line options and			            */
-/*	initialization, calls the parser end then exits.		            */
+/*                                                                      */
+/*  main2.c                                                             */
+/*  This is the main driver program for  CSC488S Assignment 2           */
+/*  It handles compiler command line options and                        */
+/*  initialization, calls the parser end then exits.                    */
 /************************************************************************/
 
 /************************************************************************/
-/*	Common definitions						                            */
+/*  Common definitions                                                  */
 /************************************************************************/
 
 #include "common.h"
@@ -15,195 +15,195 @@
 #include <getopt.h>
 
 /************************************************************************/
-/*	Shared global variables for  main2.c				                */
+/*  Shared global variables for  main2.c                                */
 /************************************************************************/
       
-	/************************************************/
-	/*  File used as a sink for error messages	    */
-	/************************************************/
+    /************************************************/
+    /*  File used as a sink for error messages      */
+    /************************************************/
 
 FILE *  errorFile ;
 
-	/************************************************/
-	/*  File used as a sink for dumps		        */
-	/************************************************/
+    /************************************************/
+    /*  File used as a sink for dumps               */
+    /************************************************/
 
 FILE *  dumpFile ;
 
-	/************************************************/
-	/*  File used as a sink for trace output	    */
-	/************************************************/
+    /************************************************/
+    /*  File used as a sink for trace output        */
+    /************************************************/
 
 FILE * traceFile ;
 
 /********************************************************/
-/*							                            */
-/*	Scanner global variables.			                */
-/*	Contain information produced by the scanner 	    */
-/*	and used by other modules			                */
-/*							                            */
+/*                                                      */
+/*  Scanner global variables.                           */
+/*  Contain information produced by the scanner         */
+/*  and used by other modules                           */
+/*                                                      */
 /********************************************************/
 
-	/**************************************************/
-	/*  Current line number in input.  Set by scanner */
-	/**************************************************/
+    /**************************************************/
+    /*  Current line number in input.  Set by scanner */
+    /**************************************************/
 
 int line ;
 
-	
+    
 
 /***********************************************************/
-/*							                               */
-/*	Control flags, set by main.c, used to cause	           */
-/*	various optional compiler actions		               */
-/*							                               */
+/*                                                         */
+/*  Control flags, set by main.c, used to cause            */
+/*  various optional compiler actions                      */
+/*                                                         */
 /***********************************************************/
 
-	/***************************************************/
-	/*  Flag used to indicate that an error occurred.  */
-	/***************************************************/
+    /***************************************************/
+    /*  Flag used to indicate that an error occurred.  */
+    /***************************************************/
 
 BOOLEAN errorOccurred ;
 
-	/************************************************/
-	/*  Cause trace of source program as it is read	*/
-	/************************************************/
+    /************************************************/
+    /*  Cause trace of source program as it is read */
+    /************************************************/
 
 BOOLEAN traceSource ;
-	
-	/************************************************/
-	/*  Cause trace of scanner token stream		    */
-	/************************************************/
+    
+    /************************************************/
+    /*  Cause trace of scanner token stream         */
+    /************************************************/
 
-BOOLEAN traceTokens ;	
+BOOLEAN traceTokens ;   
 
-	/************************************************/
-	/*  Cause trace of parser			            */
-	/************************************************/
+    /************************************************/
+    /*  Cause trace of parser                       */
+    /************************************************/
 
 #define  traceParser yydebug 
 extern int yydebug ;
 extern int yyparse(void); /* parsing */
 
 /************************************************************************/
-/*   Interface to the parser						                    */
+/*   Interface to the parser                                            */
 /************************************************************************/
 
 extern FILE*   yyin; 
 
 /************************************************************************/
-/* Default values for various files					                    */
-/* Note assumption that default files are not closed at 		        */
-/* the end of compilation						                        */
+/* Default values for various files                                     */
+/* Note assumption that default files are not closed at                 */
+/* the end of compilation                                               */
 /************************************************************************/
 
-#define DEFAULT_ERROR_FILE  	stderr
-#define DEFAULT_DUMP_FILE   	stdout
-#define DEFAULT_TRACE_FILE  	stdout
+#define DEFAULT_ERROR_FILE      stderr
+#define DEFAULT_DUMP_FILE       stdout
+#define DEFAULT_TRACE_FILE      stdout
 
 
  
 /************************************************************************/
-/* Internal utility procedure for opening optional files 		        */
+/* Internal utility procedure for opening optional files                */
 /************************************************************************/
 
 static FILE * fileOpen( char * fileName, char * fileMode, 
                                       FILE * defaultFile ){
     /****************************************************/
-    /*  open file "fileName" with mode "mode"		    */
-    /*  print error message and return default file	    */
-    /*  if open fails					                */
+    /*  open file "fileName" with mode "mode"           */
+    /*  print error message and return default file     */
+    /*  if open fails                                   */
     /****************************************************/
     FILE * fTemp ;
     if( (fTemp = fopen( fileName, fileMode )) != NULL) 
          /* successfully opened the file */
          return fTemp ;
     else {
-    	fprintf(errorFile, "Unable to open file %s\n", fileName );
-    	return defaultFile ;
+        fprintf(errorFile, "Unable to open file %s\n", fileName );
+        return defaultFile ;
     }   
 }
 
 /*********************************************************/
-/*							                             */  
-/* Main program for the Compiler			             */
-/*							                             */
+/*                                                       */  
+/* Main program for the Compiler                         */
+/*                                                       */
 /*********************************************************/
 
 int main( int argc , char *argv[] ){
     
     /****************************************************/
-    /*  Initialize global variables to default values	*/
-    /*  before processing arguments and flags		    */
+    /*  Initialize global variables to default values   */
+    /*  before processing arguments and flags           */
     /****************************************************/
     
-    char optch ;	/* sub option character	*/
-    int  ch    ;	/* option character	*/
+    char optch ;    /* sub option character */
+    int  ch    ;    /* option character */
         
     /*  Initialize files  */
 
-    yyin	= stdin ;
-    errorFile  	= DEFAULT_ERROR_FILE ;
-    dumpFile   	= DEFAULT_DUMP_FILE ;
-    traceFile  	= DEFAULT_TRACE_FILE ;
+    yyin    = stdin ;
+    errorFile   = DEFAULT_ERROR_FILE ;
+    dumpFile    = DEFAULT_DUMP_FILE ;
+    traceFile   = DEFAULT_TRACE_FILE ;
 
     /*  Initialize control flags  */
 
     errorOccurred    = FALSE ;
-    traceSource	     = FALSE ;
+    traceSource      = FALSE ;
     traceTokens      = FALSE ;
-    traceParser	     = FALSE ;
+    traceParser      = FALSE ;
     
     
     /********************************************/
-    /*	Process compiler option flags 		    */
+    /*  Process compiler option flags           */
     /********************************************/
 
-    opterr = 0 ;	/* suppress getopt error messages */
+    opterr = 0 ;    /* suppress getopt error messages */
     while( (ch = getopt( argc, argv, "T:E:R:")) != EOF){
-	    /****************************************/
-    	/* process  -ch  option			        */
-    	/****************************************/
+        /****************************************/
+        /* process  -ch  option                 */
+        /****************************************/
 
-    	switch( ch ){
-    	
-    	case 'T' :  /* Trace options  -Tilp */
-    		    while( (optch = *optarg++) ){
-    		    	/* process to end of option letters */
-    		        switch( optch ) {
-     		        case 'i' :  traceSource = TRUE ;
-    		        	    break ;
- 			case 'l' :  traceTokens = TRUE;	    
-    		        	    break ;
-    		        case 'p' :  traceParser = TRUE;
-    		    		    break ;
-    		        default:    fprintf(errorFile, 
-    		    		   "Invalid trace option %c ignored\n", optch );
-    		    		   break ;
-    		    	} ;
-    		    };
-    		    break ;
-    		    
-     	case 'E'  : /* Alternative error message file */
-    		    errorFile = fileOpen( optarg, "w", DEFAULT_ERROR_FILE );
-    		    break ;
-    		    
-   	case 'R' :  /* Alternative sink for  traces */
-   		    traceFile = fileOpen( optarg, "w", DEFAULT_TRACE_FILE );
-   		    break ;
-   		    
-        case '?' :	/* getopt error flag */
-   	default:	/* Anything else */
-   		    fprintf(stderr,"Unknown option character %c (ignored)\n",
-   		    	optch );
-   		    break ;
+        switch( ch ){
+        
+        case 'T' :  /* Trace options  -Tilp */
+                while( (optch = *optarg++) ){
+                    /* process to end of option letters */
+                    switch( optch ) {
+                    case 'i' :  traceSource = TRUE ;
+                            break ;
+            case 'l' :  traceTokens = TRUE;     
+                            break ;
+                    case 'p' :  traceParser = TRUE;
+                            break ;
+                    default:    fprintf(errorFile, 
+                           "Invalid trace option %c ignored\n", optch );
+                           break ;
+                    } ;
+                };
+                break ;
+                
+        case 'E'  : /* Alternative error message file */
+                errorFile = fileOpen( optarg, "w", DEFAULT_ERROR_FILE );
+                break ;
+                
+    case 'R' :  /* Alternative sink for  traces */
+            traceFile = fileOpen( optarg, "w", DEFAULT_TRACE_FILE );
+            break ;
+            
+        case '?' :  /* getopt error flag */
+    default:    /* Anything else */
+            fprintf(stderr,"Unknown option character %c (ignored)\n",
+                optch );
+            break ;
 
-        } ;	/* end switch( ch )	*/
+        } ; /* end switch( ch ) */
       };
      /* end of -ch argument processing */
 
     /************************************************************/
-    /*  Check for user supplied source file			            */
+    /*  Check for user supplied source file                     */
     /************************************************************/
 
     if( optind < argc ) {
@@ -213,15 +213,15 @@ int main( int argc , char *argv[] ){
      };
 
     /********************************************************/
-    /*  Compilation Starts Here				                */
+    /*  Compilation Starts Here                             */
     /********************************************************/
-     	    
+            
     fprintf(stdout,"Scanning and Parsing  begins\n");
 
 
 
     /*********************************************/
-    /*  Scan and parse the program		        */
+    /*  Scan and parse the program              */
     /*********************************************/
 
     yyparse();
@@ -230,15 +230,15 @@ int main( int argc , char *argv[] ){
     fprintf(stdout, "\nScanning and Parsing  Ends\n" );
 
     /********************************************/ 
-    /*  Clean up files if necessary		        */
+    /*  Clean up files if necessary             */
     /********************************************/
 
     if( errorFile != DEFAULT_ERROR_FILE )
-     	fclose( errorFile );
+        fclose( errorFile );
     if( dumpFile != DEFAULT_DUMP_FILE )
-    	fclose( dumpFile );
+        fclose( dumpFile );
     if( traceFile != DEFAULT_TRACE_FILE )
-    	fclose( traceFile );
+        fclose( traceFile );
   
     exit(0);
 }
